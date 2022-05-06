a315e74fd0e81acf91fcbd576fc50fa613b24785
AlxAshl <aashcheulovv@gmail.com>
2022-05-06 17:19:30 +0300

    test

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -17,6 +17,19 @@ const createEditFormTemplate = (point) => {
     </div>`
   );
 
+    const pointTypeOffer = offers
+    pointTypeOffer.offers
+    .map((offer) => `<div class="event__available-offers">
+    <div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
+      <label class="event__offer-label" for="event-offer-luggage-1">
+        <span class="event__offer-title">${offer.title}</span>
+        &plus;&euro;&nbsp;
+         <span class="event__offer-price">${offer.price}</span>
+      </label>
+    </div>`);
+
+    console.log(pointTypeOffer)
   const startTime = dateFrom !== null
     ? humanizeDate(dateFrom)
     : '';
